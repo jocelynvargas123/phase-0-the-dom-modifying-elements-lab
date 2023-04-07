5697f91e11a2e9a6300f77dd19e59086928f5a1e
Jocelyn <jocelynvargas123@yahoo.com>
2023-04-07 00:40:01 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const docMain = document.querySelector('#main')
+docMain.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Jocelyn is the champion'
